782e6c4cf7d937fec484caf481005d813694c4e6
Grzegorz Miejski <grz.miejski@gmail.com>
2014-11-16 14:15:45 +0100

    Git ignore added. Loading lines from file.

--- lab3/src/main/java/lab3/Main.java
+++ lab3/src/main/java/lab3/Main.java
@@ -1,38 +1,60 @@
 package lab3;
 
 import gogui.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import javax.json.Json;
+import javax.json.stream.JsonParser;
+import java.io.*;
+import java.util.Iterator;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        GeoList<Point> cloud = new GeoList<>();
-        Point p1 = new Point(2, 3);
-        Point p2 = new Point(3, 4);
-        Point p3 = new Point(1, 4);
+        GeoList<Line> lines = loadJsonFromFile("lab3\\src\\main\\resources\\input.json");
 
-        cloud.add(p1);
-        GoGui.snapshot();
 
-        cloud.push_back(p2);
-        GoGui.snapshot();
+    }
 
-        cloud.push_back(p3);
-        GoGui.snapshot();
-
+    private static GeoList<Line> loadJsonFromFile(String s) {
 
         GeoList<Line> lines = new GeoList<>();
 
-        Line line1 = new Line(p1, p2);
-        lines.push_back(line1);
-        GoGui.snapshot();
+        JSONParser parser = new JSONParser();
 
-        line1.setStatus(GeoObject.Status.Active);
-        GoGui.snapshot();
+        try {
 
-        GoGui.getJSON();
-        GoGui.saveJSON("C:\\home\\aaaaStudia\\Semestr_VII\\Geometria\\gogui\\visualization-grunt\\public\\data\\moje.data.json");
+            Object obj = parser.parse(new FileReader(s));
 
+            JSONArray allObjects = (JSONArray) obj;
+            Iterator<JSONObject> iterator = allObjects.iterator();
+            while (iterator.hasNext()) {
+                JSONObject next = iterator.next();
+                int lineId = Integer.valueOf(next.get("lineID").toString());
+                JSONArray points = (JSONArray) next.get("points");
+                Iterator<JSONObject> pointIterator = points.iterator();
+                JSONObject point1 = pointIterator.next();
+                JSONObject point2 = pointIterator.next();
+                int point1X = Integer.parseInt(point1.get("x").toString());
+                int point1Y = Integer.parseInt(point1.get("y").toString());
+                int point2X = Integer.parseInt(point2.get("x").toString());
+                int point2Y = Integer.parseInt(point2.get("y").toString());
 
+                Line line = new Line(new Point(point1X, point1Y), new Point(point2X, point2Y));
+                lines.push_back(line);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return lines;
     }
 }
