7a94b3fe3c574c8b5ef25c9ae35fb90cf16602db
Grzegorz Miejski <grz.miejski@gmail.com>
2014-11-16 12:59:39 +0100

    Creating jar. Lab3 module setup

---
+++ lab3/src/main/java/lab3/Main.java
@@ -0,0 +1,38 @@
+package lab3;
+
+import gogui.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        GeoList<Point> cloud = new GeoList<>();
+        Point p1 = new Point(2, 3);
+        Point p2 = new Point(3, 4);
+        Point p3 = new Point(1, 4);
+
+        cloud.add(p1);
+        GoGui.snapshot();
+
+        cloud.push_back(p2);
+        GoGui.snapshot();
+
+        cloud.push_back(p3);
+        GoGui.snapshot();
+
+
+        GeoList<Line> lines = new GeoList<>();
+
+        Line line1 = new Line(p1, p2);
+        lines.push_back(line1);
+        GoGui.snapshot();
+
+        line1.setStatus(GeoObject.Status.Active);
+        GoGui.snapshot();
+
+        GoGui.getJSON();
+        GoGui.saveJSON("C:\\home\\aaaaStudia\\Semestr_VII\\Geometria\\gogui\\visualization-grunt\\public\\data\\moje.data.json");
+
+
+    }
+}
